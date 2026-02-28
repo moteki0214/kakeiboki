b5003960ab135bd38190c7b11c97adaf4360500b
moteki0214 <2073041moteki@gmail.com>
2026-02-28 12:59:07 +0900

    繰越利益剰余金のマイナス表示を修正

--- app.js
+++ app.js
@@ -589,7 +589,8 @@ function renderBalanceSheet() {
     for (const a of store.getAccountsByType('expense')) expenseTotal += store.getBalance(a.id, dateVal);
     const netIncome = incomeTotal - expenseTotal;
     if (netIncome !== 0) {
-        bsLiabilities.innerHTML += `<tr><td>繰越利益剰余金</td><td class="col-amount">${formatCurrency(netIncome)}</td></tr>`;
+        const retainedDisplay = netIncome < 0 ? `△${formatCurrency(netIncome)}` : formatCurrency(netIncome);
+        bsLiabilities.innerHTML += `<tr><td>繰越利益剰余金</td><td class="col-amount">${retainedDisplay}</td></tr>`;
     }
 
     const netTotal = liabTotal + equityTotal + netIncome;
